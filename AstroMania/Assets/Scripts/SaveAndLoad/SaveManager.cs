cf4cdce92f363812b5d32a96e6f699a4f019a941
David Nöbauer <53915682+Ausumo@users.noreply.github.com>
2025-11-25 16:26:34 +0100

    create a web build

--- AstroMania/Assets/Scripts/SaveAndLoad/SaveManager.cs
+++ AstroMania/Assets/Scripts/SaveAndLoad/SaveManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using Unity.VisualScripting;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class SaveManager : MonoBehaviour
